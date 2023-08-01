1587fbaada516ba0632e25c1dac674ff8142c189
nach00 <bignacho@gmail.com>
2023-08-01 06:28:13 -0500

    Adds more files

---
+++ ComfyUI_windows_portable/ComfyUI/web/extensions/linkRenderMode.js
@@ -0,0 +1,10 @@
+import { app } from "../scripts/app.js";
+
+const ext = {
+	name: "pysssss.LinkRenderMode",
+	async setup(app) {
+		app.canvas.links_render_mode = 0;
+	}
+};
+
+app.registerExtension(ext);
